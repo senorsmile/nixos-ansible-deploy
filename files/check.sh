f1fb38db5d162c0b32fce4d3e6cbcd714d732dd3
Shaun Smiley <senorsmile@gmail.com>
2024-08-12 10:12:58 -0700

    add check.sh as well as lots of comments

---
+++ files/check.sh
@@ -0,0 +1,28 @@
+nix-check-upgrades-better () 
+{ 
+    if [[ $# -eq 0 ]]; then
+        echo "ERROR: must be called with path.";
+        echo "       e.g.";
+        echo "${FUNCNAME[0]} "'$(pwd)';
+        return 1;
+    fi;
+    if [[ ! -n $(command -v nvd) ]]; then
+        echo "This function requires nvd to be installed";
+        echo "You can install it temporarily with `nix-shell -p nvd`";
+    fi;
+    local flakes_params='';
+    local flakes_path="$1";
+    local flakes='True';
+    local tempdir="$flakes_path";
+    flakes_params='--flake .#';
+    tempdir=$(echo "$tempdir" | perl -plane 's|^(.*)/$|\1|');
+    pushd "$tempdir" &> /dev/null;
+    if [[ -L "$tempdir/result" ]]; then
+        rm "$tempdir/result";
+    fi;
+    eval time sudo nixos-rebuild build "$flakes_params";
+    nvd diff /run/current-system "$tempdir/result";
+    popd &> /dev/null
+}
+
+nix-check-upgrades-better "$@"
--- files/run.sh
+++ files/run.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
+set_flake='--flake .#'
 if [[ "$#" -eq 0 ]]; then
 	args="switch"
+elif [[ "$*" == *"--build-host"* ]]; then
+  set_flake=""
+	args="$@"
 else
 	args="$@"
 fi
@@ -13,4 +17,5 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${script_dir}"
 
 set -x
-time sudo nixos-rebuild "$args" --flake .#
+#time sudo nixos-rebuild "$args" --flake .#
+eval time sudo nixos-rebuild "$args" "$set_flake"
